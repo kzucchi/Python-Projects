d5ca09cf9073521e441fcd93c03350f2b5ea9d97
Kaitlyn <kzucchi@gmail.com>
2016-10-21 12:43:10 -0700

    Add files via upload
    mini program that reads text using one function 
    #reading that explains parts of a function, variables, etc.

---
+++ open function.py
@@ -0,0 +1,13 @@
+#pushing code from udacity python class week 5
+#learning about function open
+
+def read_text():
+    quotes = open (C:#file path here)
+    contents_of_file = quotes.read
+    print (contents_of_file)
+    quotes.close()
+
+#defining a function involves def then the function name ():
+#next define the variables including objects such as open
+#use python library to find out how objects work
+#call the function when action is needed
