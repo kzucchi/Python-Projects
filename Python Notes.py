336e74392e5535be1ff25b12a03e79ef63638fe3
Kaitlyn <kzucchi@gmail.com>
2016-10-27 06:38:45 -0700

    Add files via upload

---
+++ Python Notes.py
@@ -0,0 +1,14 @@
+#python notes
+
+#open returns an obeject of the file type -> see python standard library
+
+#https://docs.python.org/2/library/
+
+brad = turtle.Turtle()
+#brad is an object of Turtle
+#turtle is a function and Turtle is a class
+
+quotes = open (file_location)
+#quotes is an object of file
+#open is a stand a lone function that does not need to be initialized
+
