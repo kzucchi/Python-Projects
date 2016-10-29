91905d347e1b67db079c52a46c02cfb7f5819cb0
Kaitlyn <kzucchi@gmail.com>
2016-10-29 11:36:05 -0700

    Add files via upload
    week 5 python class. working with built in functions. Interested in the number capabilities.

---
+++ number guessing.py
@@ -0,0 +1,15 @@
+#guess the number program that 
+
+from random import randint
+
+print (randint (0,9) *2)
+
+print("guess the number")
+
+print("hint: number is multiplied by an even number")
+
+print ("hint: number is multiplied by 2")
+
+
+
+
