4d69cf9d47b14f4cdd0a12c89d11835b73e54afd
Kaitlyn <kzucchi@gmail.com>
2016-11-01 21:14:32 -0700

    Add files via upload
    Learning to define init functions in week 6 of python class. #udacity
    There's an error in my code right now and I'm going to keep learning about init to fix it. Code looks almost the same as the instructor's code, but there is a difference somewhere.

---
+++ entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://www.google.com/search?q=toy+story&biw=1440&bih=826&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiRw77al4nQAhVIwlQKHZ92BkMQ_AUIBygC#imgrc=-Yg-tF9ba2PyBM%3A",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print (toy_story.storyline)
+
+
+
