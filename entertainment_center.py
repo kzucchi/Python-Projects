f4a91bccf129093871f5af3578f136400f05c157
Kaitlyn <kzucchi@gmail.com>
2016-11-07 07:08:02 -0800

    Add files via upload
    Python media files include fresh tomatoes, entertainment center and media. When entertainment center is run it uses instances of the class from media and code instructions from fresh tomatoes to create a website and play the movie trailer for icons shown.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,15 +1,24 @@
+import fresh_tomatoes
 import media
 
 toy_story = media.Movie ("Toy Story","A story of a boy and his toys that come to life", "http://bit.ly/2fjKVGL", "http://bit.ly/VZggnq")
                         
-print (toy_story.storyline)
+#print (toy_story.storyline)
 
 avatar = media.Movie ("Avatar", "A marine lost on a island with aliens", "https://en.wikipedia.org/wiki/File:Avatar-Teaser-Poster.jpg", "http://bit.ly/2fk1rsF")
 
-print (avatar.storyline)
+#print (avatar.storyline)
 
-avatar.show_trailer()
+#avatar.show_trailer()
 
 pursuit_of_happyness = media.Movie ("Pursuit of Happyness", "A single father who pursues a career as a stockbroker", "http://bit.ly/2fodFy5", "http://bit.ly/1s2tMjQ")
 
 pursuit_of_happyness.show_trailer()
+
+#movies is an array
+
+movies = [toy_story, avatar, pursuit_of_happyness]
+
+fresh_tomates.open_movies_page(movies)
+
+
